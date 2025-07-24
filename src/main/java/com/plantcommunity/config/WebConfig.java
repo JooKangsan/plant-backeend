58537ab6608b38810fcaca04e70222450f37f3a6
JooKangSan <you04045@naver.com>
2025-07-24 19:42:28 +0900

    fix: cors 수정:

--- src/main/java/com/plantcommunity/config/WebConfig.java
+++ src/main/java/com/plantcommunity/config/WebConfig.java
@@ -2,18 +2,26 @@ package com.plantcommunity.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
+
     @Value("${file.upload-dir}")
     private String uploadDir;
-    
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadDir + "/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+                .allowedOrigins("*")  // 실제 배포 시엔 프론트 주소로 제한하는 게 좋음
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
